528a89b92b3f1063e65755a063dd29035ff8acb7
Ej1seven <erikhunter@erikhunter.dev>
2022-01-30 19:19:12 -0600

    added comments to the server

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,26 +1,29 @@
+//connects to the Stream API
 const { connect } = require("getstream");
-const bcrypt = require("bcrypt");
 const StreamChat = require("stream-chat").StreamChat;
+//library used to hash passwords
+const bcrypt = require("bcrypt");
+//performs data encryption and decryption. This is used for security purpose
 const crypto = require("crypto");
-
+//dependency module that loads environment variables from the .env file
 require("dotenv").config();
-
+//Credentials needed to login to Stream account
 const api_key = process.env.STREAM_API_KEY;
 const api_secret = process.env.STREAM_API_SECRET;
 const app_id = process.env.STREAM_APP_ID;
-
+//registers the user into my Stream account
 const signup = async (req, res) => {
   try {
     const { fullName, username, password, phoneNumber } = req.body;
-
+    //created a random userId for the user creating a new account
     const userId = crypto.randomBytes(16).toString("hex");
-
+    //connects to my Stream account using the api_key, api_secret, and app_id
     const serverClient = connect(api_key, api_secret, app_id);
-
+    //creates a hash password from the password provided by the user
     const hashedPassword = await bcrypt.hash(password, 10);
-
+    //Stream creates a token which uses the random userId created by crypto
     const token = serverClient.createUserToken(userId);
-
+    //if the post request is successful the server responds with the information provided by the user as well as the token created by crypto
     res
       .status(200)
       .json({ token, fullName, username, userId, hashedPassword, phoneNumber });
@@ -30,23 +33,24 @@ const signup = async (req, res) => {
     res.status(500).json({ message: error });
   }
 };
-
+//Logs the user into my Stream account
 const login = async (req, res) => {
   try {
     const { username, password } = req.body;
-
+    //connects to my Stream account using the api_key, api_secret, and app_id
     const serverClient = connect(api_key, api_secret, app_id);
+    //gives the client full access to the Stream API
     const client = StreamChat.getInstance(api_key, api_secret);
-
+    //searchs for the user attempting to login by matching their username which is their email
     const { users } = await client.queryUsers({ name: username });
-
+    //If the server does not find a user name it responds with a 400 error message
     if (!users.length)
       return res.status(400).json({ message: "User not found" });
-
+    //If the user is found it matches their password with the hash password using bcrypt
     const success = await bcrypt.compare(password, users[0].hashedPassword);
-
+    //Stream creates a token which uses the random userId created by crypto when the user first registered
     const token = serverClient.createUserToken(users[0].id);
-
+    //if the password match is successful the server responds with the users profile within the Stream API
     if (success) {
       res.status(200).json({
         token,
--- index.js
+++ index.js
@@ -1,13 +1,17 @@
+//server framework used to build JSON APIs
 const express = require("express");
+//node.js package used to enable CORS in various options
 const cors = require("cors");
-
+//authorization routes used to register and login users to the Stream API
 const authRoutes = require("./routes/auth");
 
 const app = express();
+
 const PORT = process.env.PORT || 5000;
-
+//dependency module that loads environment variables from the .env file
 require("dotenv").config();
-
+//Credentials linked to my Twilio account
+//Twilio is used in this app to provided users with a notification when they receive a new message through the Stream API
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const messagingServiceSid = process.env.TWILIO_MESSAGING_SERVICE_SID;
@@ -45,7 +49,7 @@ app.post("/", (req, res) => {
 
   return res.status(200).send("Not a new message request");
 });
-
+//path used to route the server to the authorization requests
 app.use("/auth", authRoutes);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
